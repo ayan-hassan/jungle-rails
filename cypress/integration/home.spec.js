b426d8ebecc7fd0ccf54b7bbafef3b662643ef4e
ayan-hassan <ahassan@hotmail.ca>
2023-06-07 00:01:35 -0400

    add test to visit homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,8 @@
+describe ("Jungle App", () => {
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000/')
+  // })
+  it('should visit the homepage', () => {
+    cy.visit('http://localhost:3000');
+  });
+})
