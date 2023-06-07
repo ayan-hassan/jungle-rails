1cf637eb840db59fc7d6328d9bccb2dc3e30d62b
ayan-hassan <ahassan@hotmail.ca>
2023-06-06 21:54:08 -0400

    update user login authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  def self.authenticate_with_credentials(email, password) 
+    user = User.find_by_email(email)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+    
+  end
 end
