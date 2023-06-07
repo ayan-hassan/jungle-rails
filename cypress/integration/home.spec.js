4e487df932fb594e289105b47dc06c5da4f22831
ayan-hassan <ahassan@hotmail.ca>
2023-06-07 00:09:01 -0400

    add test to check for all products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,4 +8,7 @@ describe ("Jungle App", () => {
   it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
 })
