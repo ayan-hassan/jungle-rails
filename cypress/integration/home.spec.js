6c9c33431db86f49910e1412ae59539ed05de567
ayan-hassan <ahassan@hotmail.ca>
2023-06-07 00:05:53 -0400

    check for products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,11 @@
 describe ("Jungle App", () => {
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000/')
-  // })
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
   it('should visit the homepage', () => {
     cy.visit('http://localhost:3000');
+  });
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
   });
 })
